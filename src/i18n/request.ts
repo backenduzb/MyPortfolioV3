53d8df591325b5a01516a2d19dd31566553e03db
Javohir <qobiljonsharipov33@gmail.com>
2025-12-08 21:31:22 +0500

    Fidex warnings

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,10 +2,13 @@ import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async ({ locale }) => {
   const supportedLocales = ["uz", "en", "ru", "ja"];
-  const currentLocale = supportedLocales.includes(locale) ? locale : "en";
+
+  const currentLocale = supportedLocales.includes(locale ?? "")
+    ? locale!
+    : "en";
 
   return {
     locale: currentLocale,
-    messages: (await import(`../../messages/${currentLocale}.json`)).default
+    messages: (await import(`../messages/${currentLocale}.json`)).default
   };
 });
